e767fb573f2b6c5fc648e975ab610fc7d598c074
florescente <campoflorescente@gmail.com>
2022-05-13 09:02:44 -0300

    unsplash

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,54 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { HiUserCircle } from 'react-icons/hi'
+import { Imagine } from './typings'
 
-const Home: NextPage = () => {
+const Home = (posts: Imagine) => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Next Now</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      Página Mestra
-      <main>
-        <HiUserCircle />
+      <main className="w-4/5 h-screen mt-10">
+        <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-3 w-full m-0 p-0">
+          {posts.results.map((dog) => (
+            <figure
+              key={dog.id + 'key'}
+              className="flex break-inside-avoid mb-4 p-0 border-2 border-white"
+            >
+              <Image
+                src={dog.urls.regular}
+                height={dog.height / 30}
+                width="100%"
+                id={dog.id}
+                key={dog.id}
+                alt="Image"
+                objectFit="fill"
+                /* placeholder="blur"
+                blurDataURL={dog.blur_hash} */
+                className="w-full	rounded"
+              />
+            </figure>
+          ))}
+        </div>
       </main>
     </div>
   )
+}
+//flex break-inside-avoid m-0
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const Access_Key = process.env.NEXT_PUBLIC_API_KEY_UNSPLASH
+  const res = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&per_page=30&query=office&client_id=${Access_Key}`
+  )
+  const posts = await res.json()
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: posts, // will be passed to the page component as props
+  }
 }
 
 export default Home
---
+++ pages/typings.d.ts
@@ -0,0 +1,166 @@
+export interface Urls {
+  raw: string
+  full: string
+  regular: string
+  small: string
+  thumb: string
+  small_s3: string
+}
+
+export interface Links {
+  self: string
+  html: string
+  download: string
+  download_location: string
+}
+
+export interface Links2 {
+  self: string
+  html: string
+  photos: string
+  likes: string
+  portfolio: string
+  following: string
+  followers: string
+}
+
+export interface ProfileImage {
+  small: string
+  medium: string
+  large: string
+}
+
+export interface Social {
+  instagram_username: string
+  portfolio_url: string
+  twitter_username: string
+  paypal_email?: any
+}
+
+export interface Sponsor {
+  id: string
+  updated_at: Date
+  username: string
+  name: string
+  first_name: string
+  last_name?: any
+  twitter_username: string
+  portfolio_url: string
+  bio: string
+  location?: any
+  links: Links2
+  profile_image: ProfileImage
+  instagram_username: string
+  total_collections: number
+  total_likes: number
+  total_photos: number
+  accepted_tos: boolean
+  for_hire: boolean
+  social: Social
+}
+
+export interface Sponsorship {
+  impression_urls: string[]
+  tagline: string
+  tagline_url: string
+  sponsor: Sponsor
+}
+
+export interface TexturesPatterns {
+  status: string
+}
+
+export interface Wallpapers {
+  status: string
+}
+
+export interface Architecture {
+  status: string
+}
+
+export interface Film {
+  status: string
+}
+
+export interface StreetPhotography {
+  status: string
+}
+
+export interface TopicSubmissions {
+  wallpapers: Wallpapers
+  architecture: Architecture
+  film: Film
+}
+
+export interface Links3 {
+  self: string
+  html: string
+  photos: string
+  likes: string
+  portfolio: string
+  following: string
+  followers: string
+}
+
+export interface ProfileImage2 {
+  small: string
+  medium: string
+  large: string
+}
+
+export interface Social2 {
+  instagram_username: string
+  portfolio_url: string
+  twitter_username: string
+  paypal_email?: any
+}
+
+export interface User {
+  id: string
+  updated_at: Date
+  username: string
+  name: string
+  first_name: string
+  last_name: string
+  twitter_username: string
+  portfolio_url: string
+  bio: string
+  location: string
+  links: Links3
+  profile_image: ProfileImage2
+  instagram_username: string
+  total_collections: number
+  total_likes: number
+  total_photos: number
+  accepted_tos: boolean
+  for_hire: boolean
+  social: Social2
+}
+
+export interface RootObject {
+  id: string
+  created_at: Date
+  updated_at: Date
+  promoted_at?: Date
+  width: number
+  height: number
+  color: string
+  blur_hash: string
+  description: string
+  alt_description: string
+  urls: Urls
+  links: Links
+  categories: any[]
+  likes: number
+  liked_by_user: boolean
+  current_user_collections: any[]
+  sponsorship: Sponsorship
+  topic_submissions: TopicSubmissions
+  user: User
+}
+
+export interface Imagine {
+  total: number
+  total_pages: number
+  results: RootObject[]
+}
