45367674712eda5fe137a0d7e00b760e9d991719
florescente <florescente@gmail.com>
2022-05-21 03:42:36 -0300

    modaltoggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,44 +2,82 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Imagine, RootObject } from '../Interfaces/typings'
 import { blurDataURLi } from '../utils/blurhash'
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import Layout from '../components/layout'
+import modal from '../utils/modal'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Home = (posts: Imagine) => {
+  const [isModal, setIsModal] = useState<boolean>(false)
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Next Now</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <main className="flex flex-col items-center w-full md:w-4/5 h-screen mt-10">
-        <h1 className="flex text-center justify-center text-2xl mb-4 font-bebas">
-          Masonry Image gallery
-        </h1>
-        <div className="columns-2 sm:columns-3 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-3 w-fit m-0 px-0 py-4">
-          {posts.results!.map((dog) => (
-            <figure
-              key={dog.id + 'key'}
-              className="flex justify-center break-inside-avoid mb-4 p-0 border-none relative"
-            >
-              <Image
-                src={dog.urls.thumb}
-                height={(dog.height / dog.width) * 210}
-                width={210}
-                id={dog.id}
-                key={dog.id}
-                alt="Image"
-                objectFit="fill"
-                placeholder="blur"
-                blurDataURL={dog.real_hash}
-                className="rounded-lg transition ease-in-out delay-150 hover:opacity-75 duration-300 cursor-zoom-in"
-              />
-            </figure>
-          ))}
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Head>
+          <title>Next Now</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <main className="flex flex-col items-center w-full md:w-4/5 h-screen mt-10">
+          <h1 className="flex text-center justify-center text-2xl mb-4 font-bebas">
+            Masonry Image gallery
+          </h1>
+          <div className="columns-2 sm:columns-3 md:columns-3 lg:columns-4 xl:columns-5 2xl:columns-6 gap-3 w-fit m-0 px-0 py-4">
+            {posts.results!.map((dog) => (
+              <figure
+                key={dog.id + 'key'}
+                className="flex justify-center break-inside-avoid mb-4 p-0 border-none relative rounded-lg"
+              >
+                <Image
+                  src={dog.urls.thumb}
+                  height={(dog.height / dog.width) * 210}
+                  width={210}
+                  id={dog.id}
+                  key={dog.id}
+                  alt="Image"
+                  objectFit="fill"
+                  placeholder="blur"
+                  blurDataURL={dog.real_hash}
+                  className="rounded-lg transition ease-in-out delay-150 hover:opacity-75 duration-300 cursor-zoom-in"
+                  onClick={() => {
+                    modal(dog.urls.regular, dog.real_hash)
+                    setIsModal(!isModal)
+                  }}
+                />
+              </figure>
+            ))}
+          </div>
+        </main>
+      </div>
+      <div
+        className={
+          isModal
+            ? 'fixed modal h-screen w-full top-0 left-0 text-cyan-100 flex flex-col'
+            : 'fixed modal h-screen w-full top-0 left-0 text-cyan-100 hidden flex-col'
+        }
+      >
+        <div className="flex justify-end basis-1/12">
+          <AiOutlineClose
+            className="flex self-center text-4xl mr-2.5"
+            onClick={() => setIsModal(!isModal)}
+          />
         </div>
-      </main>
-    </div>
+        <div className="flex justify-center items-center basis-11/12 relative">
+          <Image
+            src="https://images.unsplash.com/photo-1638913970961-1946e5ee65c4?crop=entropy\u0026cs=tinysrgb\u0026fit=max\u0026fm=jpg\u0026ixid=MnwzMjgwMTN8MXwxfGFsbHwxfHx8fHx8Mnx8MTY1MjM0MjU4MQ\u0026ixlib=rb-1.2.1\u0026q=80\u0026w=1080"
+            id="example"
+            key="example"
+            height={2160 / 4}
+            width={3840 / 4}
+            alt="example"
+            objectFit="fill"
+            className=""
+          />
+        </div>
+      </div>
+    </>
   )
 }
 
---
+++ utils/modal.ts
@@ -0,0 +1,3 @@
+export default function modal(arg: string, arg2: string) {
+  console.log(arg)
+}
